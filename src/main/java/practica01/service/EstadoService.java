fa015b20f27685123ca25b2c8f8d741c02108d75
DanielRojasF <danielrf98@gmail.com>
2023-02-26 19:34:09 -0600

    Cambiao de EstadoService

--- src/main/java/practica01/service/EstadoService.java
+++ src/main/java/practica01/service/EstadoService.java
@@ -1,11 +1,10 @@
-
 package practica01.service;
 import practica01.domain.Estado;
 import java.util.List;
 
 public interface EstadoService {
-    public List<Estado> getEstados();
-    public Estado getEstado(Estado estado);
-    public void save(Estado estado); // Para insertar o modificar (si viene el idCliente o no)
-    public void delete(Estado estado);
+public List<Estado> getEstados();
+public Estado getEstado(Estado estado);
+public void save(Estado estado); // Para insertar o modificar (si viene el idCliente o no)
+public void delete(Estado estado);
 }
