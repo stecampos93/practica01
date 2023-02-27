814d59f9aae1c9f83a1a2dfaf766ad0552780020
Stiphen Campos <Stiphen Campos@DESKTOP-02RCCK8>
2023-02-26 19:26:18 -0600

    Corregir indexController

--- src/main/java/practica01/controller/indexController.java
+++ src/main/java/practica01/controller/indexController.java
@@ -1,8 +1,9 @@
-
 package practica01.controller;
+
 import java.util.Arrays;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import practica01.domain.Estado;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,37 +12,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import practica01.dao.EstadoDao;
 import practica01.service.EstadoService;
-@Sfl4j
+
+@Slf4j
 @Controller
 
-
 public class indexController {
+
     @Autowired
-    EstadoService estadoService;
-    @GetMapping("/")
-    public String inicio(Model model) {
-       log.info("Ahora desde MVC");
-                var estados = estadoService .getEstados();
-                model.addAttribute("estados", estados);
-        return "index";
-    }
-    @GetMapping("/nuevoEstado")
-    public String nuevoEstado(Estado estado){
-        return "modificarEstado";
-    }
-    @PostMapping("/guardarEstado")
-    public String guardarEstado(Estado estado){
-        estadoService.save(estado);
-        return "redirect:/";
-    }
-    @GetMapping("moficarEstado/{idEstado}")
-    public String modificarEstado(Estado estado, Model model){
-    estado = estadoService.getEstado(estado);
-            model.addAttribute("Estado", estado);
-            return "modificarEstado";     }
-    @GetMapping("eliminarEstado/{idEstado}")
-    public String eliminarEstado(Estado estado){
-     estadoService.delete(estado);
-            return "redirect";
-    }
+    EstadoService estadoService;
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        log.info("Ahora desde MVC");
+        var estados = estadoService.getEstados();
+        model.addAttribute("estados", estados);
+        return "index";
+
+    }
+
+    @GetMapping(
+            "/nuevoEstado")
+    public String nuevoEstado(Estado estado) {
+        return "modificarEstado";
+
+    }
+
+    @PostMapping(
+            "/guardarEstado")
+    public String guardarEstado(Estado estado) {
+        estadoService.save(estado);
+        return "redirect:/";
+
+    }
+
+    @GetMapping(
+            "moficarEstado/{idEstado}")
+    public String modificarEstado(Estado estado, Model model) {
+        estado = estadoService.getEstado(estado);
+        model.addAttribute("Estado", estado);
+        return "modificarEstado";
+
+    }
+
+    @GetMapping(
+            "eliminarEstado/{idEstado}")
+    public String eliminarEstado(Estado estado) {
+        estadoService.delete(estado);
+        return "redirect";
+
+    }
 }
